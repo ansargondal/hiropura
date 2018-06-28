b93794440b68077f2e13de14b60aae23a1114395
Ansar Gondal <ansarababs395@gmail.com>
2018-06-28 18:25:14 +0500

    adds s posts composer

---
+++ app/Http/ViewComposers/PostComposer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+
+class PostComposer
+{
+    public function compose(View $view)
+    {
+//        $view->with('categories', $categories);
+    }
+}
